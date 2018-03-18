4992172e0ac778a0c9f03277bf88ca459a8d35a1
Wong Jia Hau <hou32hou@gmail.com>
2018-03-18 23:58:11 +0800

    Completed task2 partially

--- task2/index.php
+++ task2/index.php
@@ -1,5 +1,22 @@
 <html>
 <body>
 <h1>Open file in PHP</h1>
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+$file = fopen('./file.txt', 'r');
+echo "<table border='1'>";
+echo "<tr><th>Name</th><th>Email</th><th>Gender</th></tr>";
+while(!feof($file)) {
+    $fields = explode(';', fgets($file));
+    echo "<tr>";
+    foreach ($fields as $key => $value) {
+        echo "<td>$value</td>";
+    }
+    echo "</tr>";
+}
+echo "</table>";
+?>
 </body>
 </html>
